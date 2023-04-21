188844fe59b221e9f9f7aa012886c08917108e08
Daria Suntcova <batareyka23@gmail.com>
2023-04-21 14:09:40 +0300

    Pointcut v3

--- src/main/java/aop/aspects/LoggingAspect.java
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LoggingAspect {
-    @Before("execution(public void aop.UniLibrary.getBook())")
+    @Before("execution(public void get*())")
     public void beforeGetBookAdvice() {
         System.out.println("beforeGetBookAdvice: попытка получить книгу");
 
