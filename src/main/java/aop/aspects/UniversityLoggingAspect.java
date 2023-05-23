f8790b1c134dc726189160590af08d3c0c59afa9
Daria Suntcova <batareyka23@gmail.com>
2023-05-23 13:38:23 +0300

    2.11 @After

--- src/main/java/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/aop/aspects/UniversityLoggingAspect.java
@@ -1,10 +1,7 @@
 package aop.aspects;
 
 import aop.Student;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,25 +10,29 @@ import java.util.List;
 @Aspect
 public class UniversityLoggingAspect {
 
-    @Before("execution(* getStudents())")
-    public void beforeGetStudentsLoggingAdvice() {
-        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом " +
-                "getStudents");
+    //    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsLoggingAdvice() {
+//        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение списка студентов перед методом " +
+//                "getStudents");
+//    }
+//
+//    @AfterReturning(pointcut = "execution(* getStudents())",
+//            returning = "students")
+//    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
+//
+//
+//        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение списка студентов после работы" +
+//                " метода getStudents");
+//    }
+//
+//    @AfterThrowing(pointcut = "execution(* getStudents())",
+//            throwing = "exception")
+//    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception) {
+//        System.out.println("afterThrowingGetStudentsLoggingAdvice : логируем исключение " + exception );
+//    }
+    @After("execution(* getStudents())")
+    public void afterGetStudentsLoggingAdvice() {
+        System.out.println("afterGetStudentsLoggingAdvice: логируем завершение работы метода getStudents");
     }
-
-    @AfterReturning(pointcut = "execution(* getStudents())",
-            returning = "students")
-    public void afterReturningGetStudentsLoggingAdvice(List<Student> students) {
-
-
-        System.out.println("afterReturningGetStudentsLoggingAdvice: логируем получение списка студентов после работы" +
-                " метода getStudents");
-    }
-
-    @AfterThrowing(pointcut = "execution(* getStudents())",
-            throwing = "exception")
-    public void afterThrowingGetStudentsLoggingAdvice(Throwable exception) {
-        System.out.println("afterThrowingGetStudentsLoggingAdvice : логируем исключение " + exception);
-    }
-
 }
+
